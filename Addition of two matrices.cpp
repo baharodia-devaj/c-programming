d96f54abd49dac8bc93486671c64f12c45828e27
2015jamrajput <92567841+2015jamrajput@users.noreply.github.com>
2021-10-17 13:44:39 +0530

    Update Addition of two matrices.cpp

--- Addition of two matrices.cpp
+++ Addition of two matrices.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main() {
-  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
-  printf("Enter the number of rows (between 1 and 100): ");
+  int r, c, i, j;
+  printf("Enter the number of rows : ");
   scanf("%d", &r);
-  printf("Enter the number of columns (between 1 and 100): ");
+  printf("Enter the number of columns : ");
   scanf("%d", &c);
-
+int a[r][c],b[r][c], sum[r][c];
   printf("\nEnter elements of 1st matrix:\n");
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
-      printf("Enter element a%d%d: ", i + 1, j + 1);
+      
       scanf("%d", &a[i][j]);
     }
 
   printf("Enter elements of 2nd matrix:\n");
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
-      printf("Enter element b%d%d: ", i + 1, j + 1);
+      
       scanf("%d", &b[i][j]);
     }
 
@@ -28,12 +28,12 @@ int main() {
 
   // printing the result
   printf("\nSum of two matrices: \n");
-  for (i = 0; i < r; ++i)
+  for (i = 0; i < r; ++i){
     for (j = 0; j < c; ++j) {
       printf("%d   ", sum[i][j]);
-      if (j == c - 1) {
-        printf("\n\n");
-      }
+      
+
+      }printf("\n") ;
     }
 
   return 0;
